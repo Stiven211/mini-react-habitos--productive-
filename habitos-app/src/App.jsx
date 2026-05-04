5eb728da02f5522812ea99d842cf097a1b20d197
Stiven211 <kurregorojas@gmail.com>
2026-05-04 17:36:07 -0500

    Proyecto inicial creado con Vite

---
+++ habitos-app/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import './App.css'
+
+function App() {
+  const tituloProyecto = "Mis Hábitos"
+
+  return (
+    <div className="app">
+      <h1>{tituloProyecto}</h1>
+      <p>Construye tu mejor versión un día a la vez</p>
+    </div>
+  )
+}
+
+export default App
